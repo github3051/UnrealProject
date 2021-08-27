a8d2bcbb619678f8eff0744e80c2231c30f12e72
SungHoon(PC) <ksh920610@gmail.com>
2021-08-27 15:08:27 +0900

    프로젝트 모듈 추가. UnrealProjectSetting.

---
+++ Source/UnrealProjectSetting/Private/UnrealProjectSetting.cpp
@@ -0,0 +1,7 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UnrealProjectSetting.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_MODULE(FDefaultGameModuleImpl, UnrealProjectSetting);
+
---
+++ Source/UnrealProjectSetting/Public/SH_CharacterSetting.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "SH_CharacterSetting.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALPROJECTSETTING_API USH_CharacterSetting : public UObject
+{
+	GENERATED_BODY()
+	
+};
